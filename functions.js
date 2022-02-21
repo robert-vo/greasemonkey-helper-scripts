bf06b0ad52d90d1701625419e59edee7b3eb63e6
robert-vo <robert79@gmail.com>
2022-02-21 00:35:02 -0600

    refactored attemptClickElementById

--- functions.js
+++ functions.js
@@ -20,10 +20,15 @@ var maxAttempts = 50;
 var defaultTimeout = 500;
 
 const attemptClickElementById = async id => {
-    checkElementById(id).then(selector => {
-        while(maxAttempts > 0) {
-            maxAttempts--;
-            selector.click();
+    checkElementById(id).then(async (selector) => {
+        while(maxAttempts-- > 0) {
+            await wait(500);
+            if(selector.disabled !== true) {
+                selector.click();
+            } else {
+                console.log("id: %s is diabled! not clicking.", id);
+            }
         }
-    }
+        log('ran out of clicks :/ maybe try refreshing the page?');
+    })
 }
