a626c588b22964207f94b8c9c5c3150459eaa8c3
robert-vo <robert79@gmail.com>
2022-03-20 09:39:38 -0500

    add waitForElm

--- functions.js
+++ functions.js
@@ -13,6 +13,29 @@ const checkElementByQuerySelector = async selector => {
 };
 
 
+// https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
+// const elm = await waitForElm('.some-class');
+const waitForElm = selector => {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+
 const wait = async milliseconds => {
     await new Promise(resolve => setTimeout(resolve, milliseconds));
 }
