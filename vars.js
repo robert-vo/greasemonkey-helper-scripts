fd89c9884d64ca50548986bca548b1971d0d03d9
robert-vo <robert79@gmail.com>
2022-06-04 19:58:36 -0500

    add vars from net-worth script

---
+++ vars.js
@@ -0,0 +1,35 @@
+const COLORS = {
+    BLUE: '#55bae7',
+    WHITE: '#dcd7cf',
+    YELLOW: 'rgb(255, 205, 86)',
+    RED: 'rgb(255, 99, 132)',
+};
+const FONT_WEIGHT = {
+    BOLD: 'bold',
+    NORMAL: 'normal',
+};
+
+const CHANGE = 'change';
+const API_METHOD = {
+    GET: 'GET',
+    POST: 'POST',
+};
+
+const CSS_CHANGE = {
+    UP: 'up',
+    DOWN: 'down',
+    UNCHANGED: 'unchanged',
+};
+
+const API_URLS = {
+    INVESTMENTS: 'https://mint.intuit.com/pfm/v1/investments',
+    ACCOUNTS: 'https://mint.intuit.com/pfm/v1/accounts?offset=0&limit=1000',
+    TREND_DATA: 'https://mint.intuit.com/pfm/v1/trends',
+    CRYPTO_PRICE:
+        'https://min-api.cryptocompare.com/data/price?tsyms=USD&fsym=',
+    CRYPTO_PRICE_MULTIPLE:
+        'https://min-api.cryptocompare.com/data/pricemulti?tsyms=USD&fsyms=',
+};
+
+const TABLE_ROW_FONT_SIZE = 'font-size: 12px; border-bottom: 5px solid red';
+const PADDING_TOP = 'padding-top: 12px';
